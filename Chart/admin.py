f340ec77fccc8fecd7cb4f2e1e176382fe2a4769
Mohit-15 <programs.mohit15@gmail.com>
2020-09-14 18:59:33 +0530

    add user model in admin.py

--- Chart/admin.py
+++ Chart/admin.py
@@ -1,5 +1,27 @@
 from django.contrib import admin
-from .models import UserType
+from .models import UserType, User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 # Register your models here.
+class CustomUserAdmin(BaseUserAdmin):
+    model = User
+    list_display = ('email', 'name', )
+    search_fields = ('name', )
+    ordering = ('email',)
+    list_filter = ('email',)
+    fieldsets = (
+        (None, {'fields': ('email', 'name', 'password')}),
+        ('Personal', {'fields': ('mobile', 'user_type')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'name', 'is_staff', 'is_active', 'user_type')}
+        ),
+    )
+
+    filter_horizontal= ()
+
+admin.site.register(User, CustomUserAdmin)
 admin.site.register(UserType)
