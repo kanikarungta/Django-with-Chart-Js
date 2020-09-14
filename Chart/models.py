c7b3fc65a94d316af24d3f73b17d1300d6c94771
Mohit-15 <programs.mohit15@gmail.com>
2020-09-14 18:44:16 +0530

    Model created for UserType

--- Chart/models.py
+++ Chart/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.utils.translation import ugettext_lazy as _
 
 # Create your models here.
+class UserType(models.Model):
+	usertype = models.CharField(max_length = 30, unique = True)
+	user_count = models.IntegerField(default = 0)
+
+	class Meta:
+		verbose_name = _('UserType')
+		verbose_name_plural = _('UserTypes')
+
+	def __str__(self):
+		return self.usertype
+		
