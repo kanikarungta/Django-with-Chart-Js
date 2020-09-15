bdc32992c36e21b06392631b118e65bb816db18e
Mohit-15 <programs.mohit15@gmail.com>
2020-09-15 12:44:48 +0530

    base view added in which users count

--- Chart/views.py
+++ Chart/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import UserType, User
 
 # Create your views here.
 def index(request):
-	return HttpResponse("Hello")
+	ids = UserType.objects.all().values_list('id', flat = True)
+	con = ids.count()
+	for i in range(con):
+		usr = User.objects.filter(user_type = ids[i])
+		counter = usr.count()
+		role = UserType.objects.get(id = ids[i])
+		role.user_count = 0
+		role.user_count+=counter
+		role.save()
+		print("success")
+	
