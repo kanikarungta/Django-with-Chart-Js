b0a8d4b62cf95d4d63037c5c7c347dfa167566a9
Mohit-15 <programs.mohit15@gmail.com>
2020-09-16 15:29:20 +0530

    update views.py

--- Chart/views.py
+++ Chart/views.py
@@ -1,17 +1,50 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import UserType, User
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
-	ids = UserType.objects.all().values_list('id', flat = True)
-	con = ids.count()
-	for i in range(con):
-		usr = User.objects.filter(user_type = ids[i])
-		counter = usr.count()
-		role = UserType.objects.get(id = ids[i])
+	roles = UserType.objects.all()
+	ides = UserType.objects.all().values_list('id', flat = True)
+	cons = ides.count()
+	for i in range(cons):
+		usre = User.objects.filter(user_type = ides[i])
+		countr = usre.count()
+		role = UserType.objects.get(id = ides[i])
 		role.user_count = 0
-		role.user_count+=counter
+		role.user_count+=countr
 		role.save()
-		print("success")
+
+	if request.method == 'POST':
+		email = request.POST.get('email')
+		name = request.POST.get('name')
+		mobile = request.POST.get('mobile')
+		password1 = request.POST.get('password1')
+		password2 = request.POST.get('password2')
+		user_type = request.POST.get('user_type')
+
+		if password1 == password2:
+			if User.objects.filter(email = email).exists():
+				messages.info(request, "Email taken")
+				return render(request, "base.html", {'roles': roles})
+			else:
+				user = User(email=email, name = name, mobile = mobile, user_type = UserType.objects.get(id = user_type))
+				user.set_password(password1)
+				user.save()
+				ids = UserType.objects.all().values_list('id', flat = True)
+				con = ids.count()
+				for i in range(con):
+					usr = User.objects.filter(user_type = ids[i])
+					counter = usr.count()
+					role = UserType.objects.get(id = ids[i])
+					role.user_count = 0
+					role.user_count+=counter
+					role.save()
+				return render(request, 'base.html', {'roles': roles})
+		else:
+			messages.info(request, "Password do not Match")
+			return render(request, 'base.html', {'roles': roles})
+
+	return render(request, 'base.html', {'roles': roles})
 	
